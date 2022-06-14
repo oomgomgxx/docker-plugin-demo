d03905d7048c355dfe8e4a8e1d19767fb791ebeb
Chris <chris@chris.org>
2022-06-14 21:29:38 +0800

    first commit.

---
+++ src/test/java/com/example/dockerplugindemo/DockerPluginDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerplugindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerPluginDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
